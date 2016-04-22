9ebddf02bbaac95d94e79df194bec7359bd907f4
Stephen Gregg <stephenegregg@gmail.com>
2016-04-22 16:30:30 -0400

    Update test

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,9 +1,8 @@
 describe('PhoneListCtrl', function() {
   beforeEach(module('phonecatApp'));
   it('should create "phones" model with 3 phones', inject(function($controller) {
-    var scope = {},
-        ctrl = $controller('PhoneListCtrl', {$scope:scope});
-
+    var scope = {};
+    var ctrl = $controller('PhoneListCtrl', {$scope:scope});
     expect(scope.phones.length).toBe(3);
   }));
 });
