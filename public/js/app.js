5535108a48b9363a5003475342f68861439c9b22
Stephen Gregg <stephenegregg@gmail.com>
2016-04-22 14:59:12 -0400

    Add files

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,3 @@
-var app = angular.module('flapperNews', []);
+'use strict';
 
-app.controller('MainCtrl', ['$scope', function($scope) {
-
-  $scope.test = 'Hello World!';
-
-  $scope.posts = [
-    {title: 'Post 1', upvotes: Math.random()*20},
-    {title: 'Post 2', upvotes: Math.random()*20},
-    {title: 'Post 3', upvotes: Math.random()*20},
-    {title: 'Post 4', upvotes: Math.random()*20},
-    {title: 'Post 5', upvotes: Math.random()*20},
-  ]
-
-}]);
+/* App Module */
