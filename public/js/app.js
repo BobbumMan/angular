c618e7a0a69a89875e50f287dac3e010d3fa27ad
Stephen Gregg <stephenegregg@gmail.com>
2016-04-22 14:41:22 -0400

    Update client side files

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,15 @@
 var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', ['$scope', function($scope) {
+
   $scope.test = 'Hello World!';
-}])
+
+  $scope.posts = [
+    {title: 'Post 1', upvotes: Math.random()*20},
+    {title: 'Post 2', upvotes: Math.random()*20},
+    {title: 'Post 3', upvotes: Math.random()*20},
+    {title: 'Post 4', upvotes: Math.random()*20},
+    {title: 'Post 5', upvotes: Math.random()*20},
+  ]
+
+}]);
