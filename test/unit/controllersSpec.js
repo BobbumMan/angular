9550bdfddb8d0a09651091f9740b7e896ab48cfd
sgregg <stephenegregg@gmail.com>
2016-04-23 12:46:47 -0400

    Update unit tests

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,6 +3,6 @@ describe('PhoneListCtrl', function() {
   it('should create "phones" model with 3 phones', inject(function($controller) {
     var scope = {};
     var ctrl = $controller('PhoneListCtrl', {$scope:scope});
-    expect(scope.phones.length).toBe(3);
+    expect(scope.phones.length).toBe(2);
   }));
 });
