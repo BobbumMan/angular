be49dbbd642f5045eaf9e2324518fc13a82f911a
sgregg <stephenegregg@gmail.com>
2016-04-24 19:50:14 -0400

    Update protractor.conf.js

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   allScriptsTimeout: 11000,
   specs: ['e2e/*.js'],
-  capabilities: {'browserName' :'chrome'},
+  capabilities: {'browserName' :'firefox'},
   chromeOnly: true,
   baseUrl: 'http://localhost:80/',
   framework: 'jasmine',
