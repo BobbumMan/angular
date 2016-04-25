bea415bb65cb82186ea0eb9379e08d052e5664e5
Stephen Gregg <stephenegregg@gmail.com>
2016-04-25 09:42:04 -0400

    Update e2e tests

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,10 +1,7 @@
 exports.config = {
   allScriptsTimeout: 11000,
-
   specs: ['e2e/*.js'],
-  capabilities: {'browserName' :'firefox'},
-  chromeOnly: true,
-  baseUrl: 'http://localhost:80',
+  capabilities: {'browserName' :'firefox'},,
   framework: 'jasmine',
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
