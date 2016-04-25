08f50acb34ac58562f100d9164d2b8518cde7d58
sgregg <stephenegregg@gmail.com>
2016-04-24 20:09:19 -0400

    Update e2e tests

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,5 +1,6 @@
 exports.config = {
   allScriptsTimeout: 11000,
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['e2e/*.js'],
   capabilities: {'browserName' :'firefox'},
   chromeOnly: true,
