ccefe7bb802cf574a865f850b960f5ab40da411e
Stephen Gregg <stephenegregg@gmail.com>
2016-04-28 10:42:03 -0400

    edit test

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -8,6 +8,7 @@ describe('PhoneCat controllers', function() {
       toEqualData: function(util, customEqualityTesters) {
         return {
           compare: function(actual, expected) {
+            var result = {};
             result.pass = angular.equals(actual, expected);
             return result
           }
