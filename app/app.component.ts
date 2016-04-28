cca03a35975f1741857a253f9038e9793a607d56
Stephen Gregg <stephenegregg@gmail.com>
2016-04-28 16:51:07 -0400

    updates

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,16 +7,10 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1>
-    <nav>
-      <a [routerLink]="['Dashboard']">Dashboard</a>
-      <a [routerLink]="['Heroes']">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-    `,
-    styleUrls: ['app/app.component.css'],
-    directives: [ROUTER_DIRECTIVES],
-    providers: [ROUTER_PROVIDERS, HeroService]
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, HeroService]
 })
 
 @RouteConfig([
