6d75861038267855427f0eada4c22f59e48ef8b8
Stephen Gregg <stephenegregg@gmail.com>
2016-04-28 14:09:25 -0400

    Add list

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,14 @@ import {Component} from 'angular2/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div>
@@ -19,9 +27,23 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
+  public heroes = HEROES;
 }
 
 export class Hero {
   id: number;
   name: string;
 }
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
