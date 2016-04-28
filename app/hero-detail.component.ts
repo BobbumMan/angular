7073ea58cdd2465d47997a5e64e24ee7e5cc9602
Stephen Gregg <stephenegregg@gmail.com>
2016-04-28 17:00:16 -0400

    fix bug

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -11,7 +11,7 @@ import {HeroService} from './hero.service';
 
 export class HeroDetailComponent implements OnInit {
   hero: Hero;
-  constructor(private _heroService: HeroService, private _routeParams: RouteParams);
+  constructor(private _heroService: HeroService, private _routeParams: RouteParams) { };
   ngOnInit() {
     let id = +this._routeParams.get('id');
     this._heroService.getHero(id).then(hero => this.hero = hero);
