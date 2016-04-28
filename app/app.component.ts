a37507b680f3ebc6b41d36a874eb0b671410bc07
Stephen Gregg <stephenegregg@gmail.com>
2016-04-28 15:55:02 -0400

    update app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,25 @@
 import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <a [routerLink]="['Heroes']">Heroes</a>
+    <router-outlet></router-outlet>
     `,
-    directives: [HeroesComponent],
-    providers: [HeroService]
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS, HeroService]
 })
+
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
 
 export class AppComponent {
   title = "Tour of Heroes"
