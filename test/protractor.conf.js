85ac936b4374caa4df860a0f321edddfb9349593
Stephen Gregg <stephenegregg@gmail.com>
2016-04-28 10:51:54 -0400

    remove files

--- test/protractor.conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  allScriptsTimeout: 11000,
-  specs: ['e2e/*.js'],
-  capabilities: {'browserName' :'firefox'},
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
-  }
-};
